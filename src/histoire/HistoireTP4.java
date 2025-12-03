9b21d6ed5e6a5ced3c18b0b4bd16d09416f8e8de
Luc Vogel <lucvogel@MacBook-Air-De-Luc.local>
2025-12-03 13:28:12 +0100

    création de la classe Yakuza
    cancel

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -1,14 +1,14 @@
 package histoire;
 
 import personnages.Commercant;
+import personnages.Yakuza;
 
 public class HistoireTP4 {
     public static void main(String[] args) {
         Commercant marco = new Commercant("Marco", 20);
+        Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 
-        marco.direBonjour();
-        marco.seFaireExtorquer();
-        marco.recevoir(15);
-        marco.boire();
+        yaku.direBonjour();
+        yaku.extorquer(marco);
     }
 }
---
+++ src/personnages/Yakuza.java
@@ -0,0 +1,33 @@
+package personnages;
+
+public class Yakuza extends Humain {
+    private String clan;
+    private int reputation = 0;
+
+    public Yakuza(String nom, String boissonFavorite, int argent, String clan) {
+        super(nom, boissonFavorite, argent);
+        this.clan = clan;
+    }
+
+    public void extorquer(Commercant victime) {
+        parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par là ?");
+        parler(victime.getNom() + ", si tu tiens à la vie donne moi ta bourse !");
+        int argentVole = victime.seFaireExtorquer();
+        gagnerArgent(argentVole);
+        reputation++;
+        parler("J’ai piqué les " + argentVole + " sous de " + victime.getNom()
+                + ", ce qui me fait " + getArgent() + " sous dans ma poche. Hi ! Hi !");
+    }
+
+    protected void parler(String texte) {
+        System.out.println("(" + getNom() + ") - " + texte);
+    }
+
+    protected void gagnerArgent(int gain) {
+        super.gagnerArgent(gain);
+    }
+
+    protected void perdreArgent(int perte) {
+        super.perdreArgent(perte);
+    }
+}
