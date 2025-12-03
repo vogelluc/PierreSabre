6ad55574641dc98e00d87c8cfbdfbb4220c39fd0
Luc Vogel <lucvogel@MacBook-Air-De-Luc.local>
2025-12-03 13:01:19 +0100

    création de la classe Humain

---
+++ src/personnages/Humain.java
@@ -0,0 +1,50 @@
+package personnages;
+
+public class Humain {
+    private String nom;
+    private String boissonFavorite;
+    private int argent;
+
+    public Humain(String nom, String boissonFavorite, int argent) {
+        this.nom = nom;
+        this.boissonFavorite = boissonFavorite;
+        this.argent = argent;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getArgent() {
+        return argent;
+    }
+
+    private void parler(String texte) {
+        System.out.println("(" + nom + ") - " + texte);
+    }
+
+    private void gagnerArgent(int gain) {
+        argent += gain;
+    }
+
+    private void perdreArgent(int perte) {
+        argent -= perte;
+    }
+
+    public void direBonjour() {
+        parler("Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boissonFavorite + ".");
+    }
+
+    public void boire() {
+        parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+    }
+
+    public void acheter(String bien, int prix) {
+        if (argent >= prix) {
+            parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
+            perdreArgent(prix);
+        } else {
+            parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + " sous.");
+        }
+    }
+}
